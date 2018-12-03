a22e080e0c42c90339f375ad94c5ff93223556ce
Gene Boggs <gene.boggs@gmail.com>
2018-12-03 10:16:32 -0800

    WIP: Load chat.txt lines

--- lib/WS/Model/Chat.pm
+++ lib/WS/Model/Chat.pm
@@ -3,14 +3,40 @@ package WS::Model::Chat;
 use strict;
 use warnings;
 
+use Encoding::FixLatin qw( fix_latin );
+use IO::All -utf8;
+
 sub new { bless {}, shift }
 
 sub lines {
-    my ($self) = @_;
+    my ( $self, $file, $show ) = @_;
 
-    my @lines = ('1. I like pie.', '2. I want a pony!');
+    # Set the number of chat lines to show
+    $show ||= 100;
 
-    return \@lines;
+    my @content;
+
+    if ( -e $file ) {
+        my $counter = 0;
+
+        my $io = io($file);
+        $io->backwards;
+
+        while( defined( my $line = $io->getline ) ) {
+            last if ++$counter > $show;
+
+            $line = fix_latin($line);
+
+            my ( $who, $when, $what ) = ( $line =~ /^(\w+) ([T \d:-]+): (.*)$/ );
+
+            my $formatted = sprintf '<b>%s</b> <span class="smallstamp">%s:</span> %s',
+                $who, $when, $what;
+
+            push @content, $formatted;
+        }
+    }
+
+    return \@content;
 }
 
 1;
